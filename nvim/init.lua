58906b57fdca2aea441eaf5ec703bd8ba194ba20
Hadyn Youens <hadyn.youens@educationperfect.com>
2026-08-20 10:42:12 +1200

    tweak theme

--- nvim/init.lua
+++ nvim/init.lua
@@ -116,6 +116,21 @@ vim.diagnostic.config {
   },
 }
 
+require('tokyonight').setup {
+  style = 'night',
+  light_style = 'day',
+  transparent = true,
+  on_highlights = function(hl, c)
+    local prompt = c.bg_highlight
+    hl.TelescopeNormal = { bg = c.bg_dark, fg = c.fg }
+    hl.TelescopeBorder = { bg = c.bg_dark, fg = c.bg_dark }
+    hl.TelescopePromptNormal = { bg = prompt }
+    hl.TelescopePromptBorder = { bg = prompt, fg = prompt }
+    hl.TelescopePromptTitle = { bg = prompt, fg = prompt }
+    hl.TelescopePreviewTitle = { bg = c.bg_dark, fg = c.bg_dark }
+    hl.TelescopeResultsTitle = { bg = c.bg_dark, fg = c.bg_dark }
+  end,
+}
 vim.cmd.colorscheme('tokyonight')
 
 -- Native plugins
